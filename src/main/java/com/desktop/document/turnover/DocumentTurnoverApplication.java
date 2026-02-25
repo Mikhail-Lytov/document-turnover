1cac38f62e80ede36beae13299ccc2d8e35eca8d
Mihail Lytov <misha.lytov@yadex.ru>
2026-02-25 20:11:59 +0300

    added x86 support

--- src/main/java/com/desktop/document/turnover/DocumentTurnoverApplication.java
+++ src/main/java/com/desktop/document/turnover/DocumentTurnoverApplication.java
@@ -10,13 +10,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Locale;
+
 @SpringBootApplication
 public class DocumentTurnoverApplication extends Application {
 
     private ConfigurableApplicationContext applicationContext;
 
-    public static void main(String[] args) {
-        System.setProperty(LibraryLoader.JACOB_DLL_PATH, "C:\\Users\\Lytov\\Desktop\\document-turnover\\jacob-1.18-x64.dll");// TODO: Сделать динаический поиск
+    public static void main(String[] args) throws IOException {
+        String dllPath = extractJacobDllFromResources();
+        System.setProperty(LibraryLoader.JACOB_DLL_PATH, dllPath);
         launch(args);
     }
 
@@ -48,6 +57,26 @@ public class DocumentTurnoverApplication extends Application {
     public void stop() {
         if (applicationContext != null) {
             applicationContext.close();
+        }
+    }
+
+    private static String extractJacobDllFromResources() throws IOException {
+        String arch = System.getProperty("os.arch", "").toLowerCase(Locale.ROOT);
+        boolean is64 = arch.contains("64") || arch.equals("amd64") || arch.equals("x86_64");
+
+        String resourceName = is64
+                ? "/com/desktop/document/turnover/native/jacob-1.18-x64.dll"
+                : "/native/jacob-1.18-x86.dll";
+
+        try (InputStream in = DocumentTurnoverApplication.class.getResourceAsStream(resourceName)) {
+            if (in == null) {
+                throw new FileNotFoundException("Ресурс не найден: " + resourceName);
+            }
+
+            Path temp = Files.createTempFile("jacob-", ".dll");
+            Files.copy(in, temp, StandardCopyOption.REPLACE_EXISTING);
+            temp.toFile().deleteOnExit();
+            return temp.toAbsolutePath().toString();
         }
     }
 }
